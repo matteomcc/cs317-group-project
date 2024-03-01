4ba1b669b04d36cd8ede864cb9ef154e78cf7a4e
Adam Chambers SE2021 <adam.chambers.2021@uni.strath.ac.uk>
2024-03-01 13:18:47 +0000

    test

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import ProfileScreen from './screens/Profile';
+import SettingsScreen from './screens/Settings';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import SavedScreen from './screens/Saved';
+import ReferScreen from './screens/Refer';
+import DrawerItems from './constants/DrawerItems';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+ return (
+<NavigationContainer>
+<Drawer.Navigator
+       drawerType="front"
+       initialRouteName="Profile"
+       drawerContentOptions={{
+         activeTintColor: '#e91e63',
+         itemStyle: { marginVertical: 10 },
+       }}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+>
+       {
+        <Drawer.Screen
+                key={drawer.name}
+                name={drawer.name}
+                options={{
+                drawerIcon:({focused})=>
+                drawer.iconType==='Material' ?
+        <MaterialCommunityIcons
+                      name={drawer.iconName}
+                      size={24}
+                      color={focused ? "#e91e63" : "black"}
+                  />
+                :
+                drawer.iconType==='Feather' ?
+        <Feather
+                    name={drawer.iconName}
+                    size={24}
+                    color={focused ? "#e91e63" : "black"}
+                  />
+                :
+        <FontAwesome5
+                    name={drawer.iconName}
+                    size={24}
+                    color={focused ? "#e91e63" : "black"}
+                  />
+                }}
+        />
+       }
+</Drawer.Navigator>
+</NavigationContainer>
+ );
+}
