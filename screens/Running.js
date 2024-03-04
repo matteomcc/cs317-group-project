57fa5f4f6cc74810beec93c139008f012ba26956
Adam Chambers SE2021 <adam.chambers.2021@uni.strath.ac.uk>
2024-03-04 10:22:15 +0000

    Started Running page by implementing a simple button that displays a map

--- screens/Running.js
+++ screens/Running.js
@@ -1,10 +1,146 @@
-import * as React from 'react';
-import { View, Text } from "react-native";
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, View, Text, Button, Alert } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
-export default function RunningScreen() {
-   return (
-<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-<Text style={{fontSize:16,fontWeight:'700'}}>Start a run here!</Text>
-</View>
-   );
- }
+const RunningScreen = () => {
+  const [showMap, setShowMap] = useState(false);
+
+  const handleButtonPress = () => {
+    setShowMap(true);
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Button
+        title="Press me"
+        onPress={handleButtonPress}
+      />
+      {showMap && (
+        <View style={styles.container}>
+          <MapView
+            style={styles.mapStyle}
+            initialRegion={{
+              latitude: 37.78825,
+              longitude: -122.4324,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+            customMapStyle={mapStyle}
+          >
+            <Marker
+              draggable
+              coordinate={{
+                latitude: 37.78825,
+                longitude: -122.4324,
+              }}
+              onDragEnd={(e) => alert(JSON.stringify(e.nativeEvent.coordinate))}
+              title={'Test Marker'}
+              description={'This is a description of the marker'}
+            />
+          </MapView>
+        </View>
+      )}
+    </SafeAreaView>
+  );
+};
+export default RunningScreen;
+//map style taken from: https://medium.com/featurepreneur/integrating-google-maps-with-react-native-62fc8b7ecded
+const mapStyle = [
+  {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+  {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+  {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+  {
+    featureType: 'administrative.locality',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#d59563'}],
+  },
+  {
+    featureType: 'poi',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#d59563'}],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'geometry',
+    stylers: [{color: '#263c3f'}],
+  },
+  {
+    featureType: 'poi.park',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#6b9a76'}],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [{color: '#38414e'}],
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry.stroke',
+    stylers: [{color: '#212a37'}],
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#9ca5b3'}],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry',
+    stylers: [{color: '#746855'}],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [{color: '#1f2835'}],
+  },
+  {
+    featureType: 'road.highway',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#f3d19c'}],
+  },
+  {
+    featureType: 'transit',
+    elementType: 'geometry',
+    stylers: [{color: '#2f3948'}],
+  },
+  {
+    featureType: 'transit.station',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#d59563'}],
+  },
+  {
+    featureType: 'water',
+    elementType: 'geometry',
+    stylers: [{color: '#17263c'}],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [{color: '#515c6d'}],
+  },
+  {
+    featureType: 'water',
+    elementType: 'labels.text.stroke',
+    stylers: [{color: '#17263c'}],
+  },
+];
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  mapStyle: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
+
