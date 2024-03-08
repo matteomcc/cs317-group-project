9ba81731760a362db01d1cc165d57c228ef32e9d
Adam Chambers SE2021 <adam.chambers.2021@uni.strath.ac.uk>
2024-03-08 18:26:21 +0000

    Implemented a function that causes a timer(fully working) a distance(still to be done) to appear when the button is clicked

--- screens/Running.js
+++ screens/Running.js
@@ -1,24 +1,54 @@
-import React, { useState } from 'react';
-import { SafeAreaView, StyleSheet, View, Text, Button, Alert } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, View, Text, Button, Alert, Dimensions, TouchableOpacity } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
+//initialise variables and functions to change said variables
 const RunningScreen = () => {
-  const [showMap, setShowMap] = useState(false);
+  const [showStats, setStats] = useState(false);
+  const [startStop, setStartStop] = useState("Start Run");
+  const [timer, setTimer] = useState(null);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [distance, setDis] = useState(0);
 
+  //code ran when button is pressed, which starts or stops the times depending on the current state
   const handleButtonPress = () => {
-    setShowMap(true);
+    if (startStop == "Start Run") {
+      setStartStop("Stop Run")
+      setStats(true);
+      startTimer();
+    } else {
+      setStartStop("Start Run")
+      setStats(false);
+      stopTimer();
+    }
+    
   };
 
+  //starts a timer which updates the elapsedTime every second
+  const startTimer = () => {
+    const interval = setInterval(() => {
+      setElapsedTime(prevElapsedTime => prevElapsedTime + 1);
+    }, 1000);
+    setTimer(interval);
+  };
+
+  //resets the timer and stops it from increasing
+  const stopTimer = () => {
+    setElapsedTime(0);
+    clearInterval(timer);
+  };
+
+  useEffect(() => {
+    return () => clearInterval(timer);
+  }, []);
+
+  //main code, the map, buttons and stats which will appear on screen
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <Button
-        title="Press me"
-        onPress={handleButtonPress}
-      />
-      {showMap && (
         <View style={styles.container}>
           <MapView
             style={styles.mapStyle}
+            //initialRegion that the map will appear in, currently in San Francisco lol, this will be changed to the users current location once i have that working 
             initialRegion={{
               latitude: 37.78825,
               longitude: -122.4324,
@@ -27,24 +57,29 @@ const RunningScreen = () => {
             }}
             customMapStyle={mapStyle}
           >
-            <Marker
-              draggable
-              coordinate={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-              }}
-              onDragEnd={(e) => alert(JSON.stringify(e.nativeEvent.coordinate))}
-              title={'Test Marker'}
-              description={'This is a description of the marker'}
-            />
           </MapView>
-        </View>
+        </View>    
+      <View style={styles.buttonContainer}>
+      <TouchableOpacity
+          //when the button is pressed it will go to the handleButtonPress function
+          onPress={handleButtonPress}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>{startStop}</Text>
+        </TouchableOpacity>
+      </View>
+      {showStats && (
+        //if showStats is true(this meaning the Start Run button has been clicked) then Time and Distance will be displayed
+      <View style={styles.statsContainer}>
+        <Text style={styles.statsText}>Time: {elapsedTime} s</Text>
+        <Text style={styles.statsText}>Distance: {distance} km</Text>
+      </View>
       )}
     </SafeAreaView>
   );
 };
 export default RunningScreen;
-//map style taken from: https://medium.com/featurepreneur/integrating-google-maps-with-react-native-62fc8b7ecded
+//styles
 const mapStyle = [
   {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
   {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
@@ -132,6 +167,8 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
+    height: Dimensions.get('window').height * 0.5,
+    margin: 10,
     alignItems: 'center',
     justifyContent: 'flex-end',
   },
@@ -142,5 +179,33 @@ const styles = StyleSheet.create({
     right: 0,
     bottom: 0,
   },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: Dimensions.get('window').height * 0.3,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: '#1ba5c4',
+    width: Dimensions.get('window').width * 0.6,
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  statsContainer: {
+    position: 'absolute',
+    bottom: Dimensions.get('window').height * 0.2,
+    left: 0,
+    right: 0,
+    alignItems: 'center',
+  },
+  statsText: {
+    fontSize: 16,
+  }
 });
 
