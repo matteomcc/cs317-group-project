01515825a8a9abf676f4a191c8797efbf8dd7dab
5w4g-g0d <mccallumbyk@gmail.com>
2024-03-09 21:42:24 +0000

    Initial commit, week table with placeholder buttons

--- screens/Weights.js
+++ screens/Weights.js
@@ -1,10 +1,79 @@
 import * as React from 'react';
 import { View, Text } from "react-native";
 
-export default function WeightsScreen() {
-   return (
-<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-<Text style={{fontSize:16,fontWeight:'700'}}>Track your Weights here</Text>
-</View>
-   );
- }
+function clickMe(){
+    alert("Exercise Added...");
+}
+
+function WeightsScreen(){
+    return(
+        <table>
+            <tbody>
+            <tr>
+                <th>Mon</th>
+                <th>Tue</th>
+                <th>Wed</th>
+                <th>Thu</th>
+                <th>Fri</th>
+                <th>Sat</th>
+                <th>Sun</th>
+            </tr>
+            <tr>
+                <th>-----------</th>
+                <th>-----------</th>
+                <th>-----------</th>
+                <th>-----------</th>
+                <th>-----------</th>
+                <th>-----------</th>
+                <th>-----------</th>
+            </tr>
+            <tr>
+            <th>
+                    <label>
+                        Add an exercise:
+                    </label>
+                    <button onClick={clickMe}>+</button>
+                </th>
+                <th>
+                    <label>
+                        Add an exercise:
+                    </label>
+                    <button onClick={clickMe}>+</button>
+                </th>
+                <th>
+                    <label>
+                        Add an exercise:
+                    </label>
+                    <button onClick={clickMe}>+</button>
+                </th>
+                <th>
+                    <label>
+                        Add an exercise:
+                    </label>
+                    <button onClick={clickMe}>+</button>
+                </th>
+                <th>
+                    <label>
+                        Add an exercise:
+                    </label>
+                    <button onClick={clickMe}>+</button>
+                </th>
+                <th>
+                    <label>
+                        Add an exercise:
+                    </label>
+                    <button onClick={clickMe}>+</button>
+                </th>
+                <th>
+                    <label>
+                        Add an exercise:
+                    </label>
+                    <button onClick={clickMe}>+</button>
+                </th>
+            </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default WeightsScreen;
