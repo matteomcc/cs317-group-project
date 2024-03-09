a23b711224e6ce967f2b694024c874f9c2b84768
Matteo McCaughey SE2021 <matteo.mccaughey.2021@uni.strath.ac.uk>
2024-03-09 21:04:52 +0000

    implemented a selectImage function to set as a profile picture

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,11 +1,62 @@
-import * as React from 'react';
-import { View, Text } from "react-native";
+import React, { useState } from 'react';
+import { Text, View, Image, Button } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
-export default function ProfileScreen() {
-   return (
-<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-<Text style={{fontSize:16,fontWeight:'700'}}>Manage your profile here!</Text>
-    <Text style={{fontSize:16,fontWeight:'700'}}>This is a profile screen</Text>
-</View>
-   );
- }
+const ProfileScreen = () => {
+    const [image, setImage] = useState(null);
+
+    const selectImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+        });
+
+        console.log(result);
+
+
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
+        }
+    };
+    return (
+        <View>
+            <Text style={styles.heading}>User Profile</Text>
+
+            {image && <Image source={{ uri: image }} style={styles.profileImage} />}
+
+            <Button title="Choose a profile picture!" onPress={selectImage} />
+
+            <View style={styles.profileInfo}>
+                <Text style={styles.detail}>Name: John Doe</Text>
+                <Text style={styles.detail}>Email: john.doe@example.com</Text>
+
+            </View>
+        </View>
+    );
+};
+
+const styles = {
+    heading: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    profileInfo: {
+        marginVertical: 10,
+    },
+    detail: {
+        fontSize: 18,
+    },
+    profileImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        marginVertical: 10,
+        alignSelf: 'center',
+    },
+};
+
+export default ProfileScreen;
