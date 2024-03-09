f50ad9a0e868c299d0f68536e861978da9fc4c8d
Matteo McCaughey SE2021 <matteo.mccaughey.2021@uni.strath.ac.uk>
2024-03-09 18:59:41 +0000

    Initial commit for Profile branch

--- screens/Profile.js
+++ screens/Profile.js
@@ -5,6 +5,7 @@ export default function ProfileScreen() {
    return (
 <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 <Text style={{fontSize:16,fontWeight:'700'}}>Manage your profile here!</Text>
+    <Text style={{fontSize:16,fontWeight:'700'}}>This is a profile screen</Text>
 </View>
    );
  }
