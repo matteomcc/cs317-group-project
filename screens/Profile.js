8de0be068bb3a16e1ff0704ef06fe1de13f10d8a
Matteo McCaughey SE2021 <matteo.mccaughey.2021@uni.strath.ac.uk>
2024-03-09 21:45:55 +0000

    basic profile page implemented. gives default info for now. can edit with edit button. default profile picture also set

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
-import { Text, View, Image, Button } from 'react-native';
+import {Text, View, Image, Button, TextInput} from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 
 const ProfileScreen = () => {
     const [image, setImage] = useState(null);
+    const [isEditing, setIsEditing] = useState(false);
+    const [name, setName] = useState('John Doe');
+    const [email, setEmail] = useState('email@email.com');
 
     const selectImage = async () => {
-        // No permissions request is necessary for launching the image library
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
             allowsEditing: true,
             aspect: [4, 3],
             quality: 1,
         });
+
+
 
         console.log(result);
 
@@ -21,18 +25,71 @@ const ProfileScreen = () => {
             setImage(result.assets[0].uri);
         }
     };
+    const handleEdit = () => {
+        setIsEditing(true);
+    };
+
+    const handleSave = () => {
+        setIsEditing(false);
+
+    };
     return (
         <View>
-            <Text style={styles.heading}>User Profile</Text>
+            <Text style={styles.heading}>Manage your profile</Text>
 
-            {image && <Image source={{ uri: image }} style={styles.profileImage} />}
+            {image ? (
+                <Image source={{ uri: image }} style={styles.profileImage} />
+            ) : (
+                <Image
+                    source={require('../assets/icon.png')}
+                    style={styles.profileImage}
+                />
+            )}
 
             <Button title="Choose a profile picture!" onPress={selectImage} />
 
-            <View style={styles.profileInfo}>
-                <Text style={styles.detail}>Name: John Doe</Text>
-                <Text style={styles.detail}>Email: john.doe@example.com</Text>
+            {isEditing ? (
+                <TextInput
+                    style={{
+                        height: 40,
+                        borderColor: 'gray',
+                        borderWidth: 1,
+                        padding: 10,
+                        marginHorizontal: 10,
+                    }}
+                    placeholder="Name"
+                    value={name}
+                    onChangeText={(text) => setName(text)}
+                />
 
+            ) : (
+                <Text style={styles.detail}>Name: {name}</Text>
+            )}
+            {isEditing ? (
+                <TextInput
+                    style={{
+                        height: 40,
+                        borderColor: 'gray',
+                        borderWidth: 1,
+                        padding: 10,
+                        marginHorizontal: 10,
+                        marginVertical: 10,
+                    }}
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                />
+
+            ) : (
+                <Text style={styles.detail}>Email: {email}</Text>
+            )}
+
+            <View style={styles.profileInfo}>
+                {isEditing ? (
+                    <Button title="Save" onPress={handleSave} />
+                ) : (
+                    <Button title="Edit" onPress={handleEdit} />
+                )}
             </View>
         </View>
     );
@@ -43,11 +100,14 @@ const styles = {
         fontSize: 24,
         fontWeight: 'bold',
         marginBottom: 10,
+        marginHorizontal: 10,
+
     },
     profileInfo: {
         marginVertical: 10,
     },
     detail: {
+        marginHorizontal : 10,
         fontSize: 18,
     },
     profileImage: {
