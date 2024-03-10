44bb78a80ab7cbc207e0ff4ca3d5fc4632753490
5w4g-g0d <mccallumbyk@gmail.com>
2024-03-10 16:30:41 +0000

    Mobilised week page to cycle through individual days

--- screens/Weights.js
+++ screens/Weights.js
@@ -1,78 +1,95 @@
 import * as React from 'react';
-import { View, Text } from "react-native";
+import {Text, View, Image, Button, TextInput} from "react-native";
+import {useState} from "react";
 
-function clickMe(){
-    alert("Exercise Added...");
+function clickMe(value){
+    switch(value){
+        case "1":
+            alert("Bench Press added...");
+            break;
+        case "2":
+            alert("Deadlift added...");
+            break;
+        case "3":
+            alert("Squat added...");
+            break;
+        default:
+            alert("You must select an exercise first!");
+            break;
+    }
+}
+
+const styles ={
+    Button:{
+        width: "30%"
+    }
 }
 
 function WeightsScreen(){
-    return(
-        <table>
-            <tbody>
-            <tr>
-                <th>Mon</th>
-                <th>Tue</th>
-                <th>Wed</th>
-                <th>Thu</th>
-                <th>Fri</th>
-                <th>Sat</th>
-                <th>Sun</th>
-            </tr>
-            <tr>
-                <th>-----------</th>
-                <th>-----------</th>
-                <th>-----------</th>
-                <th>-----------</th>
-                <th>-----------</th>
-                <th>-----------</th>
-                <th>-----------</th>
-            </tr>
-            <tr>
-            <th>
-                    <label>
-                        Add an exercise:
-                    </label>
-                    <button onClick={clickMe}>+</button>
-                </th>
-                <th>
-                    <label>
-                        Add an exercise:
-                    </label>
-                    <button onClick={clickMe}>+</button>
-                </th>
-                <th>
-                    <label>
-                        Add an exercise:
-                    </label>
-                    <button onClick={clickMe}>+</button>
-                </th>
-                <th>
-                    <label>
-                        Add an exercise:
-                    </label>
-                    <button onClick={clickMe}>+</button>
-                </th>
-                <th>
-                    <label>
-                        Add an exercise:
-                    </label>
-                    <button onClick={clickMe}>+</button>
-                </th>
-                <th>
-                    <label>
-                        Add an exercise:
-                    </label>
-                    <button onClick={clickMe}>+</button>
-                </th>
-                <th>
-                    <label>
-                        Add an exercise:
-                    </label>
-                    <button onClick={clickMe}>+</button>
-                </th>
-            </tr>
-            </tbody>
-        </table>
+    const [day, setDay] = useState('SUN');
+    const myArray = [];
+    const lClickMe =() => {
+        switch (day) {
+            case 'SUN':
+                setDay('SAT')
+                break;
+            case 'MON':
+                setDay('SUN')
+                break;
+            case 'TUE':
+                setDay('MON')
+                break;
+            case 'WED':
+                setDay('TUE')
+                break;
+            case 'THU':
+                setDay('WED')
+                break;
+            case 'FRI':
+                setDay('THU')
+                break;
+            case 'SAT':
+                setDay('FRI')
+                break;
+        }
+    }
+
+    const rClickMe = () => {
+        switch (day) {
+            case 'SUN':
+                setDay('MON')
+                break;
+            case 'MON':
+                setDay('TUE')
+                break;
+            case 'TUE':
+                setDay('WED')
+                break;
+            case 'WED':
+                setDay('THU')
+                break;
+            case 'THU':
+                setDay('FRI')
+                break;
+            case 'FRI':
+                setDay('SAT')
+                break;
+            case 'SAT':
+                setDay('SUN')
+                break;
+        }
+    }
+
+    return (
+        <View>
+            <View style={[{flexDirection: "row", marginLeft: 20, justifyContent: 'space-evenly'}]}>
+                <Button title="<" onPress={lClickMe}/>
+                <Text style={[{marginTop: 5}]}>{day}</Text>
+                <Button title=">" onPress={rClickMe}/>
+            </View>
+            <hr style={{width: "100%"}}/>
+
+        </View>
     )
 }
 
