2d92fb80307f0829b7757e3af66263230915f45a
Abbie Diamond <abbie.diamond.2021@uni.strath.ac.uk>
2024-03-20 00:12:47 +0000

    Added name and most recent workout to home page

--- screens/Home.js
+++ screens/Home.js
@@ -1,39 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { useDarkMode } from './DarkModeContext';
-import { useLargeText } from './LargeTextContext'
+import { useLargeText } from './LargeTextContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const HomeScreen = () => {
   const { isDark } = useDarkMode();
   const { isLargeText } = useLargeText();
+  const [name, setName] = useState('');
+  const [lastWorkoutDay, setLastWorkoutDay] = useState('');
+  const [benchPressWeight, setBenchPressWeight] = useState(0);
+  const [deadliftWeight, setDeadliftWeight] = useState(0);
+  const [squatWeight, setSquatWeight] = useState(0);
 
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: 'top',
+      justifyContent: 'center',
+      alignItems: 'center',
       backgroundColor: isDark ? '#191919' : '#fff',
     },
     text: {
       color: isDark ? '#fff' : '#000',
       fontSize: isLargeText ? 24 : 18,
-      marginBottom: 5,
-      marginHorizontal: 5,
-      textAlign:'left'
-    },
-    heading: {
-      color: isDark ? '#fff' : '#000',
-      fontSize: isLargeText ? 40 : 24,
       marginBottom: 10,
-      marginHorizontal: 10,
-      textAlign:'center'
-    }
+      textAlign: 'center',
+    },
   });
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const storedName = await AsyncStorage.getItem('name');
+      const storedLastWorkoutDay = await AsyncStorage.getItem('lastWorkoutDay');
+      const storedBenchPressWeight = await AsyncStorage.getItem('benchPressWeight');
+      const storedDeadliftWeight = await AsyncStorage.getItem('deadliftWeight');
+      const storedSquatWeight = await AsyncStorage.getItem('squatWeight');
+
+      if (storedName) {setName(storedName)};
+      if (storedLastWorkoutDay) {setLastWorkoutDay(storedLastWorkoutDay)}
+      if (storedBenchPressWeight) {setBenchPressWeight(parseInt(storedBenchPressWeight))};
+      if (storedDeadliftWeight) {setDeadliftWeight(parseInt(storedDeadliftWeight))};
+      if (storedSquatWeight) {setSquatWeight(parseInt(storedSquatWeight))};
+    } catch (error) {
+      console.error('Error retrieving data:', error);
+    }
+  };
 
   return (
     <View style={styles.container}>
-        <Text style={styles.heading}>
-          Home
-        </Text>
+      <Text style={styles.text}>Name: {name}</Text>
+      <Text style={styles.text}>Last Workout Day: {lastWorkoutDay}</Text>
+      <Text style={styles.text}>Bench Press Weight: {benchPressWeight} kg</Text>
+      <Text style={styles.text}>Deadlift Weight: {deadliftWeight} kg</Text>
+      <Text style={styles.text}>Squat Weight: {squatWeight} kg</Text>
     </View>
   );
 };
